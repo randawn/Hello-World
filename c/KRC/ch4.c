150753e3273ebf237e3c116cd998f99f3282e272
randawn <saviouran@gmail.com>
2015-05-08 10:17:38 -0700

    update ch4

--- c/KRC/ch4.c
+++ c/KRC/ch4.c
@@ -37,8 +37,6 @@ int strrindex(char * s, char * t)
  * and an optionally signed exponent.
  */
 
-#include <ctype.h>
-#include <math.h>
 double atofn(char * s)
 {
     double val, vale, power;
@@ -84,10 +82,42 @@ void calculator(void)
     double op2;
     char s[MAXOP];
 
+    calhelp();
     while((type=getop(s)) != EOF) {
         switch(type) {
             case NUMBER:
                 push(atof(s)); break;
+            case CMD:
+                if(strcmp(s, "ptop")==0) {
+                    if(sp>0) printf("stack top %f\n", val[sp-1]);
+                    else     printf("WARN: stack empty\n");
+                } else if(strcmp(s, "duptop")==0) {
+                    if(sp>0 && sp<MAXVAL) {
+                        val[sp] = val[sp-1];
+                        sp++;
+                    } else if(sp==0)
+                        printf("WARN: stack empty\n");
+                    else if(sp==MAXVAL)
+                        printf("WARN: stack full\n");
+                } else if(strcmp(s, "swaptop")==0) {
+                    if(sp>1 && sp<MAXVAL) {
+                        op2 = val[sp-1];
+                        val[sp-1] = val[sp-2];
+                        val[sp-2] = op2;
+                    } else if(sp==0)
+                        printf("WARN: stack empty\n");
+                    else if(sp==MAXVAL)
+                        printf("WARN: stack full\n");
+                } else if(strcmp(s, "clrstack")==0) {
+                    sp = 0;
+                } else if(strcmp(s, "sin")==0) {
+                    push(sin(pop())); break;
+                } else if(strcmp(s, "exp")==0) {
+                    push(exp(pop())); break;
+                } else {
+                    printf("ERROR: unknow cmd str %s\n", s);
+                }
+                break;
             case '+':
                 push(pop() + pop()); break;
             case '*':
@@ -96,12 +126,20 @@ void calculator(void)
                 op2 = pop();
                 push(pop() - op2); break;
             case '/':
-                op2 = pop();
-                if(op2!=0.0)
+                if((op2=pop()) !=0.0)
                     push(pop() / op2);
                 else
                     printf("ERROR: zero divisor\n");
                 break;
+            case '%':
+                if((op2=pop()) != 0.0)
+                    push((int)pop() % (int)op2);
+                else
+                    printf("ERROR: zero mod\n");
+                break;
+            case '^':
+                op2 = pop();
+                push(pow(pop(), op2)); break;
             case '\n':
                 printf("\t%8g\n", pop()); break;
             default:
@@ -114,6 +152,7 @@ void calculator(void)
 void push(double f)
 {
     //printf("sp: %d MAXVAL %d\n", sp, MAXVAL);
+    printf("push: %f \n", f);
     if (sp < MAXVAL) {
         val[sp++] = f;
     } else {
@@ -125,6 +164,7 @@ void push(double f)
 double pop(void)
 {
     if (sp > 0) {
+        printf("pop: %f \n", val[sp-1]);
         return val[--sp];
     } else {
         printf("ERROR: stack empty\n");
@@ -135,14 +175,31 @@ double pop(void)
 // getop: get next operator or numeric operand
 int getop(char s[])
 {
-    int i, c;
+    int i=0;
+    int c;
     while((s[0]=c=getch())==' ' || c=='\t')
         ;
     s[1]='\0';
-    if(!isdigit(c) && c!='.') {
-        return c;   // NAN
+    if(!isdigit(c) && c!='.' &&
+            c!='-' && c!='+' &&
+            c!=':') {
+        return c;       // NAN
+    } else if(c==':') {
+        while(!isspace(s[i++]=c=getch()))
+            ;
+        s[i-1] = '\0';
+        return CMD;     // CMD
+    } else if(c=='+' || c=='-') {
+        s[++i]=c=getch();
+        if(isspace(c) || c==EOF) {
+            ungetch(c);
+            return s[--i];
+        } else if(!isdigit(c) && c!='.') {
+            s[i++]='\0';
+            ungetch(c);
+            return c;
+        }
     }
-    i = 0;
     if(isdigit(c))  // int part
         while(isdigit(s[++i]=c=getch()))
             ;
@@ -157,13 +214,116 @@ int getop(char s[])
 
 int getch(void)
 {
+    //return getch_();    // HACK to ungetc ex4-8
     return (bufp>0)? buf[--bufp] : getchar();
 }
 void ungetch(int c)
 {
+    //ungetch_(c); return;// HACK to ungetc ex4-8
     if(bufp >= BUFSIZE)
         printf("ERROR: ungetch: too many chars\n");
     else
         buf[bufp++] = c;
 }
+void calhelp()
+{
+    printf("reverse polish cal, input the expression\n");
+    printf("eg: 1 1 +\n");
+    printf("support cmd: (begin with :)\n");
+    printf("    ptop:     cmd print the top element of the stack\n");
+    printf("    duptop:   cmd duplicate ...\n");
+    printf("    swaptop:  cmd swap the top two element\n");
+    printf("    clrstack: cmd clear the stack\n");
+    printf("    sin: sine of x\n");
+    printf("    exp: exponential of e^x\n");
+    printf("eg: 1 :ptop\n");
+}
+
+/* Exercise 4-4
+ * Add commands to print the top element of the stack without popping,
+ * to duplicate it, and to swap the top two elements.
+ * Add a command to clear the stack. 
+ */
+
+
+/* Exercise 4-5
+ * Add access to library functions like sin, exp, and pow.
+ * See <math.h> in Appendix B, Section 4.
+ */
+
+
+/* Exercise 4-6 TODO!!! how?
+ * Add commands for handling variables.
+ * (It's easy to provide twenty-six variables with single-letter names.)
+ * Add a variable for the most recently printed value. 
+ */
+
+
+/* Exercise 4-7
+ * Write a routine ungets(s) that will
+ * push back an entire string onto the input.
+ * Should ungets know about buf and bufp,
+ * or should it just use ungetch ? 
+ */
+
+void ungets(char * s)
+{
+    int i = strlen(s);
+    ungetch(EOF);   // ATTN: EOF != NUL
+    while(i>=0) {
+        ungetch(*(s+i-1));
+        i--;
+    }
+}
+
+
+/* Exercise 4-8
+ * Suppose there will never be more than one character of pushback.
+ * Modify getch and ungetch accordingly. 
+ */
+
+char bufc = EOF;
+int getch_(void)
+{
+    if (bufc==EOF) {
+        return getchar();
+    } else {
+        int temp;
+        temp = bufc;
+        bufc = EOF;
+        return temp;
+    }
+}
+void ungetch_(int c)
+{
+    if(bufc==EOF)
+        printf("ERROR: ungetch: char occupyed\n");
+    else {
+        bufc = c;
+    }
+}
+
+/* Exercise 4-9
+ * Our getch and ungetch do not handle a pushed-back EOF correctly.
+ * Decide what their properties ought to be if an EOF is pushed BACK,
+ * then implement your design.
+ */
+
+// change :char buf[BUFSIZE];
+// to     :int buf[BUFSIZE];
+// FIXME: char work fine on my mechine,
+//        unsigned char not work. WHY???
+
+
+/* Exercise 4-10
+ * An alternate organization uses getline to read an entire input line;
+ * this makes gech and ungetch unnecessary.
+ * Revise the calculator to use this approach.
+ */
+
+// TODO: 
+// read an entire line, put in a char[].
+// getop use pointer to access. do not need getch and ungetch.
+
+
 
--- c/KRC/ch4.h
+++ c/KRC/ch4.h
@@ -1,13 +1,19 @@
 
 #include <stdio.h>
+#include <string.h>
 
 int strrindex(char * s, char * t);      // ex 4-1
 double atofn(char * s);                 // ex 4-2
-                                        // ex 4-3
-#include <stdlib.h> // atof()
 #include <ctype.h>
+#include <math.h>
+                                        // ex 4-3
+                                        // ex 4-4
+                                        // ex 4-5
+                                        // ex 4-6 TODO
+#include <stdlib.h> // atof()
 #define MAXOP   100 // max size of operand or operator
 #define NUMBER  '0' // signal that a num was found
+#define CMD     '1' // signal that a cmd was found
 #define MAXVAL 100  // max depth of val stack
 #define BUFSIZE 100
 void calculator(void);
@@ -16,4 +22,7 @@ void push(double);
 double pop(void);
 int getch(void);
 void ungetch(int);
-
+void calhelp();
+void ungets(char * s);                  // ex 4-7
+int getch_(void);                         // ex 4-8
+void ungetch_(int);
