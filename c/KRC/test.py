dffc9ee836c94f912d6109d6014ff0726b80f5f6
randawn <saviouran@gmail.com>
2015-06-09 09:46:41 -0700

    clean

--- c/KRC/test.py
+++ c/KRC/test.py
@@ -2,14 +2,3 @@
 
 import cwrap as c
 
-
-year = 2001
-month= 1
-day  = 28
-print "year ", year, "month", month, "day", day
-
-yearday = c.day_of_year(year, month, day);
-print "the ", yearday, "day of year", year
-
-month, day = c.month_day(year, yearday);
-print "month", month, "day", day
